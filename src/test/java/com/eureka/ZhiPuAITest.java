ff5a7b8def4886556285fefcf0ad8dfe7c61faa1
Eurekan <2455096251@qq.com>
2025-02-22 19:05:19 +0800

    deepseek测试

--- src/test/java/com/eureka/ZhiPuAITest.java
+++ src/test/java/com/eureka/ZhiPuAITest.java
@@ -3,14 +3,13 @@ package com.eureka;
 import com.eureka.common.ErrorCode;
 import com.eureka.config.ZhiPuAIManager;
 import com.eureka.exception.BusinessException;
-import com.zhipu.oapi.service.v4.model.ChatMessage;
-import com.zhipu.oapi.service.v4.model.ChatMessageRole;
+import com.zhipu.oapi.service.v4.model.*;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ZhipuAITest {
+public class ZhiPuAITest {
     @Resource
     private ZhiPuAIManager zhiPuAIManager;
 
@@ -18,7 +17,7 @@ public class ZhipuAITest {
     public void testZhiPuAI() {
         String response;
         try {
-            response = zhiPuAIManager.doChat(new ChatMessage(ChatMessageRole.USER.value(), "请为我讲解搏击俱乐部这部电影的内容？"));
+            response = zhiPuAIManager.doChat(new ChatMessage(ChatMessageRole.USER.value(), "中国20到25岁男人平均身高是多少？"));
         } catch (Exception e) {
             throw new BusinessException(ErrorCode.THIRD_SERVICE_ERROR);
         }
